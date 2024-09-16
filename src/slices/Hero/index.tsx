8f28051f5a562a9e7eb54de2362eb535a670077d
gourab8389 <deyg6988@gmail.com>
2024-09-16 13:55:11 +0530

    Add Hero slice with primary fields and update model and mocks

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,6 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Hero`.
@@ -15,7 +16,16 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for hero (variation: {slice.variation}) Slices
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.subheading} />
+      <PrismicRichText field={slice.primary.body} />
+      {slice.primary.button_text}
+      <PrismicNextLink field={slice.primary.button_link}>
+        Link
+      </PrismicNextLink>
+      <PrismicNextImage field={slice.primary.cans_image} />
+      <PrismicRichText field={slice.primary.second_heading} />
+      <PrismicRichText field={slice.primary.second_body} />
     </section>
   );
 };
