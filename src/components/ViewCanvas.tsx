bf4a6779e9e4b7627951aa0759891f5984e2389e
gourab8389 <deyg6988@gmail.com>
2024-09-16 20:51:57 +0530

    Add FloatingCan component for animated soda can; update ViewCanvas with floating effects and environment settings

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,46 +1,52 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { View } from "@react-three/drei";
+import { Environment, Float, View } from "@react-three/drei";
 import { Suspense } from "react";
 import dynamic from "next/dynamic";
 import { SodaCan } from "./Sodacan";
 
 const Loader = dynamic(
-  () => import("@react-three/drei").then((mod) => mod.Loader),
-  { ssr: false },
+    () => import("@react-three/drei").then((mod) => mod.Loader),
+    { ssr: false },
 );
 
 type Props = {};
 
-export default function ViewCanvas({}: Props) {
-  return (
-    <>
-      <Canvas
-        style={{
-          position: "fixed",
-          top: 0,
-          left: "50%",
-          transform: "translateX(-50%)",
-          overflow: "hidden",
-          pointerEvents: "none",
-          zIndex: 30,
-        }}
-        shadows
-        dpr={[1, 1.5]}
-        gl={{ antialias: true }}
-        camera={{
-          fov: 30,
-        }}
-      >
-        {/* <Suspense fallback={null}>
-          <View.Port />
-        </Suspense> */}
-        <SodaCan/>
-        <ambientLight intensity={2}/>
-        <spotLight intensity={3} position={[1, 1, 1]}/>
-      </Canvas>
-      {/* <Loader /> */}
-    </>
-  );
+export default function ViewCanvas({ }: Props) {
+    return (
+        <>
+            <Canvas
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    overflow: "hidden",
+                    pointerEvents: "none",
+                    zIndex: 30,
+                }}
+                shadows
+                dpr={[1, 1.5]}
+                gl={{ antialias: true }}
+                camera={{
+                    fov: 30,
+                }}
+            >
+                {/* <Suspense fallback={null}>
+                <View.Port />
+                </Suspense> */}
+                <Float
+                    speed={1}
+                    rotationIntensity={2}
+                    floatIntensity={1}
+                    floatingRange={[-0.1, 0.1]}
+                >
+                    <SodaCan />
+                </Float>
+                <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5} />
+            </Canvas>
+            {/* <Loader /> */}
+        </>
+    );
 }
