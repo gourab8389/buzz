fcd6fa5b32f331f98203a8220072cac375c45fb7
gourab8389 <deyg6988@gmail.com>
2024-09-16 20:29:15 +0530

    Add SodaCan model and integrate into ViewCanvas; update camera settings and structure

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,37 +1,46 @@
-"use client"
+"use client";
 
-import { Canvas } from "@react-three/fiber"
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+import { SodaCan } from "./Sodacan";
 
+const Loader = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.Loader),
+  { ssr: false },
+);
 
+type Props = {};
 
-type Props = {}
-
-export default function ViewCanvas({ }: Props) {
-    return (
-        <Canvas
-            style={{
-                position: "fixed",
-                top: 0,
-                left: "50%",
-                transform: "translateX(-50%)",
-                overflow: "hidden",
-                pointerEvents: "none",
-                zIndex: 30,
-
-            }}
-            shadows
-            dpr={[1, 1.5]}
-            gl={{antialias: true}}
-            camera={{
-                fov: 100,
-            }}
-        >
-            <mesh rotation={[0.5, 0.5, 0]} position={[1, 0, 0]}>
-                <boxGeometry />
-                <meshStandardMaterial color={"hotpink"} />
-            </mesh>
-            <ambientLight intensity={2} />
-            <spotLight intensity={3} position={[1, 1, 1]}/>
-        </Canvas>
-    );
+export default function ViewCanvas({}: Props) {
+  return (
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          overflow: "hidden",
+          pointerEvents: "none",
+          zIndex: 30,
+        }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+        camera={{
+          fov: 30,
+        }}
+      >
+        {/* <Suspense fallback={null}>
+          <View.Port />
+        </Suspense> */}
+        <SodaCan/>
+        <ambientLight intensity={2}/>
+        <spotLight intensity={3} position={[1, 1, 1]}/>
+      </Canvas>
+      {/* <Loader /> */}
+    </>
+  );
 }
