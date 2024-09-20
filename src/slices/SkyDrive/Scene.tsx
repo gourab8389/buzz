a634469f370b7c493f40d833b234b34bbb5372e5
gourab8389 <deyg6988@gmail.com>
2024-09-20 21:16:58 +0530

    Add Scene component and rename SkyDrive to SkyDive; update structure for improved rendering

---
+++ src/slices/SkyDrive/Scene.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Scene({}: Props) {
+  return (
+    <div>Scene</div>
+  )
+}
+
+export default Scene
--- src/slices/SkyDrive/index.tsx
+++ src/slices/SkyDrive/index.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
+import { Bounded } from "@/components/Bounded";
+// import Scene from "./Scene";
+import { View } from "@react-three/drei";
 /**
  * Props for `SkyDrive`.
  */
@@ -9,15 +14,22 @@ export type SkyDriveProps = SliceComponentProps<Content.SkyDriveSlice>;
 /**
  * Component for "SkyDrive" Slices.
  */
-const SkyDrive = ({ slice }: SkyDriveProps): JSX.Element => {
+const SkyDive = ({ slice }: SkyDriveProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="skydive h-screen"
     >
-      Placeholder component for sky_drive (variation: {slice.variation}) Slices
-    </section>
+      <h2 className="sr-only">{slice.primary.sentence}</h2>
+      <View className="h-screen w-screen">
+        {/* <Scene
+          flavor={slice.primary.flavor}
+          sentence={slice.primary.sentence}
+        /> */}
+      </View>
+    </Bounded>
   );
 };
 
-export default SkyDrive;
+export default SkyDive;
